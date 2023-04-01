fdb6dc5da580762fcacadce1cadad0a97f767b01
Roman Efremov <9992014290@mail.ru>
2023-04-01 10:42:53 +0300

    /s

--- src/main/java/javaCoursec/lambdaExpression/Task_1.java
+++ src/main/java/javaCoursec/lambdaExpression/Task_1.java
@@ -4,6 +4,7 @@ public class Task_1 {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 4, 2, 54, 52, 22};
         System.out.println(arr.length);
+        System.out.println(arr[0]);
 
     }
 }
